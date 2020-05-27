e0482cfa198c1284dc93c2448463fc1303a92dc1
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-05-27 14:39:39 -0500

    initial commit

---
+++ transitions/svelte-demo/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Topic from './Topic.svelte';
+
+  const topics = [
+    {
+      title: 'CSS Transitions',
+      description: `
+        CSS transitions provide a way to control
+        animation speed when changing CSS properties.
+        Instead of having property changes take effect immediately,
+        you can cause the changes in a property
+        to take place over a period of time.
+      `
+    },
+    {
+      title: 'CSS Flexible Box Layout',
+      description: `
+        CSS Flexible Box Layout is a module of CSS that defines a CSS box model
+        optimized for user interface design, and the layout of items in
+        one dimension.  In the flex layout model, the children of a
+        flex container can be laid out in any direction, and can “flex” their
+        sizes, either growing to fill unused space or shrinking to avoid
+        overflowing the parent.  Both horizontal and vertical alignment of
+        the children can be easily manipulated.
+      `
+    }
+  ];
+</script>
+
+{#each topics as {title, description}}
+  <Topic {title} {description} />
+{/each}
+
+<style>
+  :global(body) {
+    background-color: linen;
+    font-family: sans-serif;
+    padding: 1rem;
+  }
+
+  @media (max-width: 640px) {
+    :global(body) {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+</style>
---
+++ transitions/svelte-demo/src/Topic.svelte
@@ -0,0 +1,74 @@
+<script>
+  export let title;
+  export let description;
+
+  let show = false;
+
+  $: classes = 'topic' + (show ? ' show' : '');
+
+  const handleClick = () => (show = !show);
+</script>
+
+<div class="{classes}">
+  <header>
+    <div class="title">{title}</div>
+    <button class="toggle" on:click="{handleClick}">&#x25b6;</button>
+  </header>
+  <section>{description}</section>
+</div>
+
+<style>
+  .topic {
+    box-shadow: 5px 5px 5px #666;
+    display: inline-block;
+    background-color: cornflowerblue;
+    border-radius: 1rem;
+    color: white;
+    margin: 0 1rem 1rem 0;
+    padding: 1rem;
+    vertical-align: top;
+    width: 350px;
+  }
+
+  .topic > header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  .topic > header > button {
+    background-color: transparent;
+    border: none;
+    color: inherit;
+    font-size: 2rem;
+    margin: 0;
+    outline: none;
+    transition-duration: 0.5s;
+    transition-property: transform;
+    transform: rotate(0deg);
+  }
+
+  .topic.show > header > button {
+    transform: rotate(90deg);
+  }
+
+  .topic > section {
+    background-color: orange;
+    margin-top: 0;
+    max-height: 0;
+    padding: 0 0.5rem;
+    overflow-y: hidden;
+    transition-duration: 0.5s;
+    transition-property: margin-top, max-height, padding;
+  }
+
+  .topic.show > section {
+    margin-top: 1rem;
+    max-height: 100vh;
+    padding-bottom: 0.5rem;
+    padding-top: 0.5rem;
+  }
+</style>
